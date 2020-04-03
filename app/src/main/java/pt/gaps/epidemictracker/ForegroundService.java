35d9853ebc00b76f367a98509de2caf6beba8a5a
gaps <gaps@no.email>
2020-04-03 13:24:13 +0100

    Bluetooth LE advertise and scan helpers initialization in the foreground service

--- app/src/main/java/pt/gaps/epidemictracker/ForegroundService.java
+++ app/src/main/java/pt/gaps/epidemictracker/ForegroundService.java
@@ -1,10 +1,17 @@
 package pt.gaps.epidemictracker;
 
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 
 public class ForegroundService extends Service {
+    private BluetoothLeAdvertiseHelper bleAdvertiseHelper;
+    private BluetoothLeScanHelper bleScanHelper;
+
     public ForegroundService() {
     }
 
@@ -19,6 +26,27 @@ public class ForegroundService extends Service {
 
         startForeground(NotificationHelper.FOREGROUND_NOTIFICATION_ID,
                 NotificationHelper.createForegroundNotification(this));
+
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager != null ? bluetoothManager.getAdapter() : null;
+
+        if (bluetoothAdapter == null) {
+            // Bluetooth is not supported
+            stopSelf();
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            bleAdvertiseHelper = new BluetoothLeAdvertiseHelperLollipop(bluetoothAdapter);
+            bleScanHelper = new BluetoothLeScanHelperLollipop(bluetoothAdapter);
+        } else {
+            bleAdvertiseHelper = new BluetoothLeAdvertiseHelperStub();
+            bleScanHelper = new BluetoothLeScanHelperStub();
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            // TODO: Prompt user to turn on Bluetooth
+        }
     }
 
     @Override
